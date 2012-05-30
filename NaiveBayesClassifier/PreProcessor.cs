0e1886db5a1e6d316db9b5cc4903a82e3f585545
Austin Valeske <avaleske@gmail.com>
2012-05-30 01:31:03 -0700

    Wrote vocabulary generator
    Also added data files to folder.

---
+++ NaiveBayesClassifier/PreProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace NaiveBayesClassifier
+{
+     class PreProcessor
+     {
+          SortedSet<string> vocabulary = new SortedSet<string>();
+          SortedSet<string> stopList = new SortedSet<string>();
+
+          public void GenerateVocabulary(string sourceFileName, string stopListFileName)
+          {
+               
+               using (var stopListFile = new StreamReader(stopListFileName))
+               {
+                    string line;
+                    while ((line = stopListFile.ReadLine()) != null)
+                    {
+                         stopList.Add(line.Trim().ToLower());
+                    }
+               }
+
+               using (var sourceFile = new StreamReader(sourceFileName))
+               {
+                    string line;
+                    while((line = sourceFile.ReadLine()) != null)
+                    {
+                         var words = line.ToLower().Trim().Split(' ');
+                         foreach (var word in words)
+                         {
+                              if(!stopList.Contains(word.Trim()))
+                              {
+                                   //SortedSets take care of duplicates
+                                   vocabulary.Add(word.Trim());
+                              }
+                         }
+                    }
+               }
+          }
+     }
+}
--- NaiveBayesClassifier/Program.cs
+++ NaiveBayesClassifier/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace NaiveBayesClassifier
 {
@@ -9,6 +10,41 @@ namespace NaiveBayesClassifier
      {
           static void Main(string[] args)
           {
+               string stoplist;
+               string trainingData;
+               string trainingLabel;
+               string testingData;
+               string testingLabel;
+
+               try
+               {
+                    foreach (var file in args)
+                    {
+                         if(!File.Exists(file))
+                              throw new FileNotFoundException(string.Format("The file {0} could not be found.", file));
+                    }
+
+                    stoplist = args[0];
+                    trainingData = args[1];
+                    trainingLabel = args[2];
+                    testingData = args[3];
+                    testingLabel = args[4];
+               }
+               catch (Exception e)
+               {
+
+                    Console.WriteLine("There was a problem reading one or more of the input files.");
+                    Console.WriteLine("Double check your file paths and try again.");
+                    Console.WriteLine(e.Message);
+                    exit();
+               }
+          }
+
+          private static void exit()
+          {
+               Console.WriteLine("Press any key to close.");
+               Console.ReadKey();
+               System.Environment.Exit(0);
           }
      }
 }
